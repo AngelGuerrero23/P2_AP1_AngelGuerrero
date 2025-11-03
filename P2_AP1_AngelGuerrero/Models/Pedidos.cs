8f11cd66c86e7a5c7796eea6eba110a9fd064fe6
angel <angel_guerrero1@ucne.edu.do>
2025-11-03 18:51:41 -0400

    Pedidos, PedidosDetalle, Componente y contexto agregado.

---
+++ P2_AP1_AngelGuerrero/Models/Pedidos.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P2_AP1_AngelGuerrero.Models;
+
+public class Pedidos
+{
+    [Key]
+    public int PedidoId { get; set; }
+
+    [Required]
+    public DateTime Fecha { get; set; }= DateTime.Now;
+    [Required(ErrorMessage ="El nombre del cliente es obligatorio")]
+    public string NombreCliente { get; set; }
+    [Range(0, int.MaxValue)]
+    public double Total { get; set; }
+
+    [ForeignKey("PedidoId")]
+    public virtual ICollection<PedidosDetalle> GetPedidosDetalles { get; set; } = new List<PedidosDetalle>();
+}
